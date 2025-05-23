b5ef734de1f5549e8d5ba4e66498d63afafa0fe9
efariday <ergashevafarida0630@gmail.com>
2025-05-23 19:35:23 +0500

    Initial commit

---
+++ products/api_endpoints/CategoryList/views.py
@@ -0,0 +1,10 @@
+
+from rest_framework import serializers
+
+from products.models import Category
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "slug"]
